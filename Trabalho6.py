bfa16ce061e57d48dec0286f7479fef300b99460
diogoserenoo <160735789+diogoserenoo@users.noreply.github.com>
2024-03-20 09:38:45 -0300

    Add files via upload

---
+++ Trabalho6.py
@@ -0,0 +1,35 @@
+print ("Diogo")
+
+numero = 18
+nome = "Diogo"
+booleano = True
+
+numero1 = 15
+numero2 = 28
+numero3 = 37
+numero4 = 42
+frase = "A IBMEC fica na Barra da Tijuca"
+palavra = "Flamengo"
+
+media = (numero1 + numero2 + numero3 + numero4) / 4
+
+quadrado_numero = numero3 ** 2
+
+dobro_numero = numero4 * 2
+
+quantidade_letras = len(palavra)
+
+quantidade_espacos = frase.count(" ")
+
+primeiro_maior = numero1 > numero2
+
+maior_numero = max(numero1, numero2, numero3, numero4)
+
+print("Média aritmética dos números:", media)
+print("Quadrado do terceiro número:", quadrado_numero)
+print("Dobro do quarto número:", dobro_numero)
+print("Quantidade de letras na palavra:", quantidade_letras)
+print("Quantidade de espaços em branco na frase:", quantidade_espacos)
+print("O primeiro número é maior que o segundo?", primeiro_maior)
+print("O maior número é:", maior_numero)
+
